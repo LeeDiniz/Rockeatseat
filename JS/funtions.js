7777df105afcec982ee7ec9408d58184db91fc7b
LeeDiniz <leticiadiniz1237@gmail.com>
2023-04-22 10:07:53 -0300

    Function

---
+++ JS/funtions.js
@@ -0,0 +1,12 @@
+//Frases motivacionais 
+
+function createPhrases () {
+    console.log ('As estrelas estão atrás das nuvens, mas elas continuam ali')
+    console.log('Mais que obstáculos difíceis, existem dias difíceis')
+    console.log('Confie no que a vida planejou para você')
+}
+createPhrases ()
+createPhrases ()
+createPhrases ()
+createPhrases ()
+console.log ('fim do programa')
