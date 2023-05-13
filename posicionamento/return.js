294d09a4329da49d4a27a2ba93ee624fe9d9ce9f
LeeDiniz <leticiadiniz1237@gmail.com>
2023-05-13 11:54:27 -0300

    Update return.js

--- posicionamento/return.js
+++ posicionamento/return.js
@@ -5,8 +5,8 @@ const sum = function(number1, number2){
 
 let number1 = 35
 let number2 = 15
-sum(number1, number2)
+let total = number1 + number2
 
 console.log(`O número 1 é ${number1}`)
 console.log(`O número 2 é ${number2}`)
-console.log(`A soma é ${sum(number1,number2)}`)
+console.log(`A soma é ${total}`)
