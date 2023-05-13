c39d022c84459205d6ed859b95cf5b0dffdff72a
LeeDiniz <leticiadiniz1237@gmail.com>
2023-05-13 11:09:36 -0300

    funtions

--- JS/argumento.js
+++ JS/argumento.js
@@ -3,3 +3,4 @@ console.log (number1 + number2)
 }
 
 sum(2,3)
+sum(3254,1000)
---
+++ JS/retornando.js
@@ -0,0 +1,11 @@
+const sum = function (number1,number2) {
+    console.log (number1 + number2)
+    }
+    
+   let number1 = 15
+   let number2 = 22
+   sum(number1,number2)
+return sum
+   console.log(`o numero 1 é ${number1}`)
+   console.log(`o numero 1 é ${number2}`)
+   console.log(`a soma é ${sum(number1,number2)}`)
