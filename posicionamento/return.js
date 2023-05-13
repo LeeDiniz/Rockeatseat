8e3fff5e7b2533399a46821f145129b33d4fa571
LeeDiniz <leticiadiniz1237@gmail.com>
2023-05-13 11:19:29 -0300

    return

---
+++ posicionamento/return.js
@@ -0,0 +1,11 @@
+const sum = function(number1, number2){
+    console.log(number1 + number2)
+}
+
+let number1 = 35
+let number2 = 15
+sum(number1, number2)
+
+console.log(`o numero 1 é ${number1}`)
+console.log(`o numero 2 é ${number2}`)
+console.log(`a soma é ${sum(number1,number2)}`)
