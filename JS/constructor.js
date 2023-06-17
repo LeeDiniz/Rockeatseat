8b312a24947d0420b7282a5ebb7e56bdb980f2de
LeeDiniz <leticiadiniz1237@gmail.com>
2023-06-17 12:06:28 -0300

    Create constructor.js

---
+++ JS/constructor.js
@@ -0,0 +1,10 @@
+function Person() {
+    this.name = name
+    this.walk = function(){
+        return "andando"
+    }
+}
+const leticia = new Person(Leticia)
+const cristina = new Person(Cristina)
+console.log(leticia)
+console.log(cristina)
